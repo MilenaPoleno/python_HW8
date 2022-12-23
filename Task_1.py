a60c00aa0bfd9ebb6ade9119fe4264e1ce7d30df
MilenaPoleno <112932828+MilenaPoleno@users.noreply.github.com>
2022-12-23 15:37:47 +0300

    add Task_1

--- Task_1.py
+++ Task_1.py
@@ -24,19 +24,35 @@ add() для реализации операции
 
 
 class Matrix:
+    
     def __init__(self, list_1):
         self.list_1 = list_1
 
     def __str__(self):
         return str('\n'.join(' '.join(str(el) for el in i) for i in self.list_1))
+    def __add__(self, other):
+        try:    
+            result = []
+            numbers = []
+            for i in range(len(self.list_1)):
+                for j in range(len(self.list_1[0])):
+                    summa = other.list_1[i][j] + self.list_1[i][j]
+                    numbers.append(summa)
+                    if len(numbers) == len(self.list_1):
+                        result.append(numbers)
+                        numbers = []
+            return Matrix(result)
+        except IndexError:
+            return "Размеры матриц не совпадают. Операция невозможна!"
 
-    def __add__(self, list_2):
-        for i in range(len(self.list_1)):
-            for j in range(len(list_2.list_1[i])):
-                self.list_1[i][j] = self.list_1[i][j] + list_2.list_1[i][j]
-        return Matrix.__str__(self)
+m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m3 = Matrix([[1, 2, 3], [4, 5, 6]])
+print(m1 + m2)
+print(m1 + m3)
 
 
-m_1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-m_2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(m_1.__add__(m_2))
+  
+m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(m1 + m2)
